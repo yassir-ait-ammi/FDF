e0612e80549085a06425c1ef160f949b2c1ccc0c
Yassir Ait Ammi <yaait-am@e2r10p9.1337.ma>
2024-12-25 11:22:44 +0100

    go

---
+++ Makefile
@@ -0,0 +1,10 @@
+
+CC = cc
+CFLAGS = -Wall -Wextra -Werror
+MLX_DIR = ../minilibx-linux
+MLX = $(MLX_DIR)/libmlx.a
+
+
+all : $(NAME)
+
+$(NAME) : 
---
+++ fdf.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaait-am <yaait-am@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/25 11:09:11 by yaait-am          #+#    #+#             */
+/*   Updated: 2024/12/25 11:11:34 by yaait-am         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+#define FDF_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 9
+
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strchr(char *s, int c);
+char	*ft_strdup(char *src);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strcat(char *dest, char *src);
+char	*ft_strcpy(char *dest, char *src);
+int		ft_strlen(char *s);
+char	*ft_after_new_line(char *s);
+char	*ft_return(char *s);
+
+#endif
